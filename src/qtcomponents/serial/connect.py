a4fa86d107a060231e282cf4a0624a4eb6411dc2
proximalf <proximalfuture@gmail.com>
2025-09-29 09:52:54 +0100

    strip addresss

--- src/qtcomponents/serial/connect.py
+++ src/qtcomponents/serial/connect.py
@@ -9,14 +9,14 @@ class SerialConnectionWidget(QWidget):
     """
     A WidgetComponent for managing serial connections.
 
-    A ComboBox populated with avalible ports, a button to connect, a spinbox for an address, starting at 11, and a status label.
+    A ComboBox populated with avalible ports, a button to connect, and a status label.
 
     When the `Connect` button is pressed a `connection_requested` signal is emitted.
-    The signal emits the `connection_flag` status, and address value.
+    The signal emits the port and `connection_flag` status.
     When connected the `connection_flag` will be set to `True`.
     """
 
-    signal_connection_requested = Signal(bool, str, int)
+    signal_connection_requested = Signal(bool, str)
 
     def __init__(self, parent: QWidget, *args, **kwargs) -> None:
         super().__init__(parent=parent, *args, **kwargs)
@@ -31,23 +31,11 @@ class SerialConnectionWidget(QWidget):
         self.connect_button = QPushButton(self)
         self.port_cbox = QComboBox(self)
 
-        self.address_layout = QHBoxLayout()
-        self.address_spinbox = QSpinBox(self)
-        self.address_spinbox.setMinimum(11)
-        self.address_spinbox.setMaximum(99)
-        self.address_label = QLabel(self)
-        self.address_label.setText("Address: ")
-        self.address_label.setFixedWidth(65)
-
-        self.address_layout.addWidget(self.address_label)
-        self.address_layout.addWidget(self.address_spinbox)
-
         self.label = QLabel(self)
 
         self.connection_layout.addWidget(self.connect_button)
         self.connection_layout.addWidget(self.port_cbox)
-        self.connection_layout.addLayout(self.address_layout)
-
+        
         self.horizontalLayout.addLayout(self.connection_layout)
         self.horizontalLayout.addWidget(self.label)
 
@@ -76,11 +64,9 @@ class SerialConnectionWidget(QWidget):
         self.connect_button.setText("Connect" if not connected else "Disconnect")
         self.port_cbox.setEnabled(not connected)
         self.connect_button.setEnabled(not connected)
-        self.address_spinbox.setEnabled(not connected)
 
     def emit_connection_request(self) -> None:
         self.signal_connection_requested.emit(
             self._connected_flag,
             self.port_cbox.currentText(),
-            self.address_spinbox.value(),
         )
